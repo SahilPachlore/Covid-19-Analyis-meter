219d45df8c2c811443ce01c3fc9013de6099d4a6
Sahil Pachlore <sahilpachlore2020@gmail.com>
2021-09-27 10:12:24 -0700

    Initail Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function updateMap(){
+fetch("/data.json")
+.then(response =>response.json())
+.then(rsp =>{
+    console.log(rsp.data);
+    console.log("We are updating Map")
+    rsp.data.forEach(element =>{
+        latitude=element.latitude;
+        longitude = element.longitude;
+        cases=element.infected;
+        if (cases>255){
+            color="rgb(255,0,0)"
+        }
+        else{
+            color=`rgb(${cases},0,0)`
+        }
+        //Mark on the Map
+        new mapboxgl.Marker({
+            draggable: false,
+            color:color
+            })
+            .setLngLat([longitude, latitude])
+            .addTo(map);
+    })
+})
+
+}
+let interval=2000;
+setInterval(updateMap,interval);
